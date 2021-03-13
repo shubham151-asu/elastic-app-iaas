b72e28ac763e019cb8bb2a7ff78edeb0c479121c
Sandeep Sunny <sandeepsunny@vmware.com>
2021-03-12 21:24:55 -0700

    Commented deployment controller

--- deployment-controller/src/main/java/com/example/myapp/App.java
+++ deployment-controller/src/main/java/com/example/myapp/App.java
@@ -43,6 +43,7 @@ import software.amazon.awssdk.services.ec2.model.*;
 
 
 public class App {
+    // Setting all the required variables that will be used throughout
     final static Region REGION = Region.US_EAST_2;
     final static String REQUEST_QUEUE_NAME = "autoscaler-request-queue";
     final static String RESPONSE_QUEUE_NAME = "autoscaler-response-queue";
@@ -62,6 +63,7 @@ public class App {
         SqsClient sqsClient = SqsClient.builder().region(REGION).build();
         Ec2Client ec2Client = Ec2Client.builder().region(REGION).build();
 
+        // Creating an instance of the velocity template engine
         VelocityEngine ve = new VelocityEngine();
         ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath"); 
         ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
@@ -77,26 +79,17 @@ public class App {
                             break;
         }
         
-
-        // System.out.println("\nUploading object...");
-
-        // s3Client.putObject(PutObjectRequest.builder().bucket(projectInstances.bucketName).key(s3BucketKey)
-        //                 .build(),
-        //         RequestBody.fromString("Testing with the AWS SDK for Java"));
-
-        // System.out.println("Upload complete");
-        // System.out.printf("%n");
-
-        
         System.out.println("Exiting...");
     }
 
+    // Reads the web-tier.sh template, replaces all the required values, base64 encodes the script
     public static String readUserDataFile(VelocityEngine ve, String filePath, String inputBucketName, String outputBucketName,
     String requestQueueUrl, String responseQueueUrl) throws IOException {
         return Base64.getEncoder().encodeToString(replaceValuesInTemplate(ve, filePath,
             inputBucketName, outputBucketName, requestQueueUrl, responseQueueUrl).getBytes());
     }
 
+    // Sets the environment variables in the web-tier.sh template
     public static String replaceValuesInTemplate(VelocityEngine ve, String filePath, String inputBucketName, String outputBucketName,
     String requestQueueUrl, String responseQueueUrl) throws IOException {
         Template t = ve.getTemplate(filePath);
@@ -111,6 +104,7 @@ public class App {
         return writer.toString();
     }
 
+    // Launches all the required cloud resources for the project
     public static ProjectInstances launchProject(S3Client s3Client, SqsClient sqsClient, Ec2Client ec2Client, VelocityEngine ve)
             throws IOException {
         String requestQueueUrl = createSQSQueue(sqsClient, REQUEST_QUEUE_NAME);
@@ -119,8 +113,6 @@ public class App {
         String outputBucketName = createS3Bucket(s3Client, S3_BUCKET_NAME_OUTPUT);
         String webInstanceId = createEc2Instance(ec2Client, WEB_TIER_AMI_ID, "web-tier", 
         readUserDataFile(ve, WEB_TIER_USER_DATA_FILE_PATH, inputBucketName, outputBucketName, requestQueueUrl, responseQueueUrl), S3_IAM_ROLE);
-        // String appInstanceId = createEc2Instance(ec2Client, APP_TIER_AMI_ID, "app-tier", 
-        // readUserDataFile(ve, CONTROLLER_USER_DATA_FILE_PATH, inputBucketName, outputBucketName, requestQueueUrl, responseQueueUrl), S3_IAM_ROLE);
         
         ProjectInstances projectInstances = new ProjectInstances.ProjectInstancesBuilder()
             .inputBucketName(inputBucketName)
@@ -128,13 +120,12 @@ public class App {
             .requestQueueUrl(requestQueueUrl)
             .responseQueueUrl(responseQueueUrl)
             .webInstanceId(webInstanceId)
-            // .controllerInstanceId(controllerInstanceId)
-            // .appInstanceId(appInstanceId)
             .build();
         
         return projectInstances;
     }
 
+    // Created S3 bucket with specified name
     public static String createS3Bucket(S3Client s3Client, String bucketName) {
         s3Client.createBucket(CreateBucketRequest
                 .builder()
@@ -153,6 +144,7 @@ public class App {
         return bucketName;
     }
 
+    // Created SQS queue with specified name
     public static String createSQSQueue(SqsClient sqsClient, String queueName) {
         System.out.println("\nCreating Queue");
         CreateQueueRequest createQueueRequest = CreateQueueRequest.builder()
@@ -166,6 +158,7 @@ public class App {
         return queueUrl;
     }
 
+    // Created EC2 instance with specified AMI id, name, userdata and IAM role
     public static String createEc2Instance(Ec2Client ec2Client, String amiId, String name, String userData, String iamRole) {
         RunInstancesRequest runRequest = RunInstancesRequest.builder()
             .imageId(amiId)
@@ -202,10 +195,12 @@ public class App {
         return "";
     }
 
+    // Cleans up created S3 buckets, SQS queues and EC2 instances
     public static void cleanUp(S3Client s3Client, SqsClient sqsClient, Ec2Client ec2Client) {
         System.out.println("Cleaning up...");
         deleteS3Buckets(s3Client);
         deleteSQSQueues(sqsClient);
+        deleteEc2Instances(ec2Client);
         System.out.println("Closing the connection to Amazon S3");
         s3Client.close();
         System.out.println("Closing the connection to Amazon SQS");
@@ -217,6 +212,7 @@ public class App {
         System.out.printf("%n");
     }
 
+    // Deletes all the S3 buckets
     public static void deleteS3Buckets(S3Client s3Client) {
         ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder().build();
         ListBucketsResponse listBucketsResponse = s3Client.listBuckets(listBucketsRequest);
@@ -235,6 +231,7 @@ public class App {
         }
     }
 
+    // Deletes all the objects inside the specified S3 bucket
     public static void deleteObjectsInBucket(S3Client s3Client, String bucketName) {
         ListObjectsV2Request listObjectsV2Request = ListObjectsV2Request.builder().bucket(bucketName).build();
         ListObjectsV2Response listObjectsV2Response;
@@ -254,6 +251,7 @@ public class App {
         } while(listObjectsV2Response.isTruncated());
     }
 
+    // Deletes all the created SQS queues
     public static void deleteSQSQueues(SqsClient sqsClient) {
         ListQueuesRequest listQueuesRequest = ListQueuesRequest.builder().build();
         ListQueuesResponse listQueuesResponse = sqsClient.listQueues(listQueuesRequest);
@@ -272,4 +270,28 @@ public class App {
         }
     }
 
+    // Deletes all the created EC2 instances
+    public static void deleteEc2Instances(Ec2Client ec2Client) {
+        System.out.println("Deleting EC2 instances...");
+        List<String> instanceIds = getRunningEc2InstanceIds(ec2Client);
+        TerminateInstancesRequest terminateInstancesRequest = TerminateInstancesRequest.builder()
+        .instanceIds(instanceIds)
+        .build();
+        TerminateInstancesResponse response = ec2Client.terminateInstances(terminateInstancesRequest);
+    }
+
+    // Retrieves all the running EC2 instances
+    public static List<String> getRunningEc2InstanceIds(Ec2Client ec2Client) {
+        Filter runningFilter = Filter.builder().name("instance-state-name").values("running", "pending", "stopped").build();
+        DescribeInstancesRequest describeInstancesRequest = DescribeInstancesRequest.builder().filters(runningFilter).build();
+        DescribeInstancesResponse describeInstanceResponse = ec2Client.describeInstances(describeInstancesRequest);
+        
+        List<String> appInstanceIds = new ArrayList<String>();
+        for (Reservation reservation : describeInstanceResponse.reservations()) {
+            for (Instance instance : reservation.instances()) {
+                appInstanceIds.add(instance.instanceId());
+            }
+        }
+        return appInstanceIds;
+    }
 }
